8f317a4c2515dee08c7b5b6bee249c2bda4ae9d8
Sam H. Smith <sam.henning.smith@protonmail.com>
2023-04-18 13:31:39 +0200

    stubbed list graph

---
+++ eclipse_proj/ListGraph.java
@@ -0,0 +1,57 @@
+import java.util.Collection;
+import java.util.Set;
+import java.util.HashSet;
+
+public class ListGraph<T> {
+	public void add(T node)
+	{
+		
+	}
+	
+	public void connect(T node1, T node2, String name, int weight)
+	{
+		
+	}
+	
+	public void setConnectionWeight(T node1, T node2, int weight)
+	{
+		
+	}
+	
+	public Set<T> getNodes()
+	{
+		return new HashSet<T>();
+	}
+	/*
+	public Collection<Edge<T>> getEdgesFrom(T node)
+	{
+		
+	}
+	*/
+	/*
+	public Edge<T> getEdgeBetween(T noed1, T node2)
+	{
+		
+	}
+	*/
+	public void disconnect(T node1, T node2)
+	{
+		
+	}
+	
+	public void remove(T node)
+	{
+		
+	}
+	
+	public boolean pathExist(T from, T to)
+	{
+		return false;
+	}
+	/*
+	public List<Edge<T>> getPath(T from, T to)
+	{
+		
+	}
+	*/
+}
