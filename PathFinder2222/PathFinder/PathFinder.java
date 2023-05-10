5e671a59ab7c216e7caa6ddd44d1bfaac7b10f1e
Berngarn <71653254+Berngarn@users.noreply.github.com>
2023-05-10 08:37:49 +0200

    Lita ändringar

--- PathFinder2222/PathFinder/PathFinder.java
+++ PathFinder2222/PathFinder/PathFinder.java
@@ -1,34 +1,112 @@
 package PathFinder;
- 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
- 
-public class PathFinder extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class PathFinder extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private boolean unsavedChanges = false;
+
+    // Swing-komponenter här
+    private JPanel mapPanel;
+    private JMenuBar menuBar;
+    private JMenu fileMenu;
+    private JMenuItem newMapItem, openItem, saveItem, saveImageItem, exitItem;
+
+    public PathFinder() {
+        // Initialize components and add them to the window
+        setTitle("PathFinder");
+        setSize(850, 500);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+        // Create and add mapPanel
+        mapPanel = new JPanel();
+        getContentPane().add(mapPanel, BorderLayout.CENTER);
+
+        // Create File menu and add it to JMenuBar
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        newMapItem = new JMenuItem("New Map");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        saveImageItem = new JMenuItem("Save Image");
+        exitItem = new JMenuItem("Exit");
+
+        fileMenu.add(newMapItem);
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(saveImageItem);
+        fileMenu.add(exitItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+
+        // Add ActionListener for menu items
+        newMapItem.addActionListener(e -> newMap());
+        openItem.addActionListener(e -> open());
+        saveItem.addActionListener(e -> save());
+        saveImageItem.addActionListener(e -> saveImage());
+        exitItem.addActionListener(e -> exit());
+        addWindowListener(new WindowAdapter() {
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            public void windowClosing(WindowEvent e) {
+                exit();
             }
         });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+    }
+
+    // Methods for handling menu items
+    private void newMap() {
+        try {
+            BufferedImage image = ImageIO.read(new File("src/europa.gif"));
+            JLabel label = new JLabel(new ImageIcon(image));
+            mapPanel.removeAll();
+            mapPanel.add(label);
+            this.setSize(image.getWidth(), image.getHeight());
+            this.validate();
+            this.repaint();
+            unsavedChanges = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void open() {
+        // 
+    }
+
+
+    private void save() {
+        //
+    }
+
+    private void saveImage() {
+        //
+    }
+    private void exit() {
+        if (unsavedChanges) {
+            int result = JOptionPane.showConfirmDialog(this,
+                    "Det finns osparade ändringar. Är du säker på att du vill avsluta?",
+                    "Bekräftelse av avslut",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.WARNING_MESSAGE);
+
+            if (result == JOptionPane.NO_OPTION) {
+                return;
+            }
+        }
+        System.exit(0);
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            PathFinder app = new PathFinder();
+            app.setVisible(true);
+        });
     }
 }
+
