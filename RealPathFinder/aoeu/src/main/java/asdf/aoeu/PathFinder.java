4b64e96faeba73338575f967636637a81957f209
Sam H. Smith <sam.henning.smith@protonmail.com>
2023-05-25 14:21:51 +0200

    Vi är bra på programering

--- RealPathFinder/aoeu/src/main/java/asdf/aoeu/PathFinder.java
+++ RealPathFinder/aoeu/src/main/java/asdf/aoeu/PathFinder.java
@@ -1,20 +1,69 @@
 package asdf.aoeu;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Optional;
+
+import javax.imageio.ImageIO;
+
+class Place {
+	public double x, y;
+	public boolean selected;
+	public String name;
+	public long s_number;
+	public Place(double x, double y, String name) {
+		this.x = x;
+		this.y = y;
+		this.name = name;
+		selected = false;
+		s_number = 0;
+	}
+}
+
+class Connection {
+	public Place p1, p2;
+	public String name;
+	public int time;
+	public Connection(Place p1, Place p2, String name, int time) {
+		this.p1 = p1;
+		this.p2 = p2;
+		this.name = name;
+		this.time = time;
+	}
+}
 
 public class PathFinder extends Application {
-    private boolean unsavedChanges = false;
+    private static final double CIRCLE_RAD = 25;
+
+	private static final double LINE_WIDTH = 5;
+
+	private boolean unsavedChanges = false;
 
     private BorderPane layout;
     private MenuBar menuBar;
@@ -25,6 +74,13 @@ public class PathFinder extends Application {
     
     private VBox vert_box;
     private Stage primaryStage;
+
+	private boolean new_place_mode;
+
+	private Group circles;
+	private ArrayList<Place> places = new ArrayList<Place>();
+	private long s_counter = 0;
+	private ArrayList<Connection> connections = new ArrayList<Connection>();
 
     @Override
     public void start(Stage primaryStage) {
@@ -71,6 +127,9 @@ public class PathFinder extends Application {
         newPlaceButton.setOnAction(e -> newPlace());
         newConnectionButton.setOnAction(e -> newConnection());
         changeConnectionButton.setOnAction(e -> changeConnection());
+        
+        this.circles = new Group();
+        layout.getChildren().add(circles);
 
         Scene scene = new Scene(layout);
         primaryStage.setTitle("PathFinder");
@@ -78,11 +137,41 @@ public class PathFinder extends Application {
         primaryStage.show();
         this.primaryStage = primaryStage;
     }
+    
+    private void updatePlaces()
+    {
+    	this.circles.getChildren().clear();
+        for (int i = 0; i < places.size(); i++) {
+        	Circle c;
+        	if(places.get(i).selected)
+        	{
+        		c = new Circle(places.get(i).x, places.get(i).y, CIRCLE_RAD, Paint.valueOf("RED"));
+        	} else {
+        		c = new Circle(places.get(i).x, places.get(i).y, CIRCLE_RAD, Paint.valueOf("BLUE"));
+        	}
+        	c.setOnMouseClicked(e -> placePress(e.getSource()));
+			circles.getChildren().add(c);
+		}
+        
+        Canvas can = new Canvas(primaryStage.getScene().getWidth(), primaryStage.getScene().getHeight());
+        can.mouseTransparentProperty().set(true);
+        circles.getChildren().add(can);
+        
+        GraphicsContext gc = can.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(LINE_WIDTH);
+        for (int i = 0; i < connections.size(); i++) {
+        	Place p1 = connections.get(i).p1;
+        	Place p2 = connections.get(i).p2;
+			gc.strokeLine(p1.x, p1.y, p2.x, p2.y);
+		}
+    }
 
     private void newMap() {
         try {
-            Image image = new Image(new FileInputStream("europa.gif"));
+            Image image = new Image(new URL("file:europa.gif").openStream());
             ImageView imageView = new ImageView(image);
+            imageView.setOnMouseClicked(e -> mouseMapPress(e.getX(), e.getY()));
             if(vert_box.getChildren().size() == 1) {
             	vert_box.getChildren().add(imageView);
             } else {
@@ -90,21 +179,31 @@ public class PathFinder extends Application {
             }
             primaryStage.sizeToScene();
             unsavedChanges = true;
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void open() {
-        //
+        System.out.println("Open file."); // TODO: open
     }
 
     private void save() {
-        //
+        System.out.println("Save file."); // TODO: save
     }
 
     private void saveImage() {
-        //
+    	WritableImage snapshot = this.primaryStage.getScene().snapshot(null);
+    	try {
+			while(snapshot.getProgress() < 1.0) {}
+		    BufferedImage bi = SwingFXUtils.fromFXImage(snapshot, null);
+		    File f = new File("capture.png");
+		    ImageIO.write(bi, "png", f);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+    	
     }
 
     private void exit() {
@@ -120,8 +219,15 @@ public class PathFinder extends Application {
             System.exit(0);
         }
     }
+    
+    private void resetState() {
+    	this.primaryStage.getScene().setCursor(Cursor.DEFAULT);
+    	this.new_place_mode = false;
+    	this.newPlaceButton.setDisable(false);
+    }
 
     private void findPath() {
+    	resetState();
         //
     }
 
@@ -130,11 +236,112 @@ public class PathFinder extends Application {
     }
 
     private void newPlace() {
-        //
+    	resetState();
+    	this.primaryStage.getScene().setCursor(Cursor.CROSSHAIR);
+    	this.new_place_mode = true;
+    	this.newPlaceButton.setDisable(true);
+    }
+    
+    private void mouseMapPress(double mx, double my) {
+    	System.out.println("Press " + mx + " " + my);
+    	
+    	if(this.new_place_mode)
+    	{
+    		TextInputDialog dialog = new TextInputDialog("");
+    		dialog.setTitle("Name");
+    		dialog.setHeaderText(null);
+    		dialog.setContentText("Please enter your name:");
+    		Optional<String> res = dialog.showAndWait();
+    		if (!res.isPresent()) {
+				resetState();
+				return;
+			}
+    		places.add(new Place(mx, my, res.get()));
+    		updatePlaces();
+    		resetState();
+    	}
+    }
+    
+    private void placePress(Object o)
+    {
+    	int index = circles.getChildren().indexOf(o);
+    	System.out.println("Pressed index = " + index);
+    	
+    	int count = 0;
+    	for (int i = 0; i < places.size(); i++) {
+			if(places.get(i).selected)
+			{ count += 1; }
+		}
+
+    	if(count == 2 && !places.get(index).selected)
+    	{ return; }
+    	
+    	places.get(index).selected = !places.get(index).selected;
+    	places.get(index).s_number = ++this.s_counter;
+    	updatePlaces();
     }
 
     private void newConnection() {
-        //
+    	{
+        	int count = 0;
+        	for (int i = 0; i < places.size(); i++) {
+    			if(places.get(i).selected)
+    			{ count += 1; }
+    		}
+        	if(count != 2)
+        	{
+        		Alert a = new Alert(AlertType.ERROR, "Two places must be selected.");
+        		a.setTitle("Error!");
+        		a.setHeaderText(null);
+        		a.showAndWait();
+        		return;
+        	}
+    	}
+    	
+    	Place p1 = null; Place p2 = null;
+    	int i = 0;
+    	while(i < places.size()) {
+			if(places.get(i).selected)
+			{ p1 = places.get(i); i++; break; }
+			i++;
+		}
+    	while(i < places.size()) {
+			if(places.get(i).selected)
+			{ p2 = places.get(i); i++; break; }
+			i++;
+		}
+    	if(p1.s_number > p2.s_number)
+    	{
+    		Place t = p1;
+    		p1 = p2;
+    		p2 = t;
+    	}
+    	System.out.println("NC " + p1.name + " -> " + p2.name);
+    	
+    	TextInputDialog dialog = new TextInputDialog("");
+		GridPane grid = new GridPane();
+		grid.setAlignment(null);
+		grid.setPadding(new Insets(10));
+		
+		TextField nameField = new TextField();
+		TextField timeField = new TextField();
+		
+		grid.addRow(1, new Label("Name: "), nameField);
+		grid.addRow(2, new Label("Time: "), timeField);
+    	
+		dialog.getDialogPane().setContent(grid);
+    	dialog.setHeaderText(null);
+    	dialog.setTitle("Connection");
+    	boolean was_confirm = dialog.showAndWait().isPresent();
+    	if(!was_confirm)
+    	{ return; }
+    	
+    	// TODO parse and error handling
+    	
+    	String connection_name = "dave";
+    	int connection_time = 5;
+    	connections.add(new Connection(p1, p2, connection_name, connection_time));
+    	updatePlaces();
     }
 
     private void changeConnection() {
--- RealPathFinder/aoeu/src/main/java/module-info.java
+++ RealPathFinder/aoeu/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module asdf.aoeu {
     requires javafx.controls;
 	requires javafx.graphics;
 	requires java.desktop;
+	requires javafx.swing;
     exports asdf.aoeu;
 }
